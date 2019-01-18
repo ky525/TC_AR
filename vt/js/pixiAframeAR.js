2c10f07fe7e2a3652978628a00f5a65b3975f1e8
ky525 <yusuke_ilk525@hotmail.com>
2019-01-19 01:07:02 +0900

    縦表示の位置、大きさ調整

--- vt/js/pixiAframeAR.js
+++ vt/js/pixiAframeAR.js
@@ -146,10 +146,10 @@ window.onload = function () {
 		var plane = document.createElement('a-plane');
 		plane.setAttribute('plane', '');
 		plane.setAttribute('color', '#000');
-		plane.setAttribute('height', '5');
-		plane.setAttribute('width', '5');
+		plane.setAttribute('height', '4');
+		plane.setAttribute('width', '4');
 		//マーカーを基準にしたモデルの相対位置
-		plane.setAttribute('position', '1.4 1.0 0');
+		plane.setAttribute('position', '1.4 0.5 0');
 		var stand = stand_mode ? '0 0 0' : '-90 0 0';
 		plane.setAttribute('rotation', stand);
 		marker.appendChild(plane);
